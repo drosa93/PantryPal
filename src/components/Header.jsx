b58f8ac230a16aa82ba57853d1cc6527de813647
drosa93 <DM_delarosa93@yahoo.com>
2025-06-04 11:27:15 -0500

    pantry pal alt

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <>
         <header className='header'>
-            <img className='chef-logo' src='./images/PantryPal.png' alt='chef-claude-logo'/>
+            <img className='chef-logo' src='./images/PantryPal.png' alt='PantryPal-logo'/>
             <h1>Pantry Pal</h1>
             
         </header>
